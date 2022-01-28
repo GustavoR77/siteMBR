0754c509dac18569db624ce7938603d6561394a9
Gustavo Ribeiro <52572546+GustavoR77@users.noreply.github.com>
2022-01-28 14:33:21 -0300

    update

--- web/src/components/Video/index.js
+++ web/src/components/Video/index.js
@@ -4,18 +4,16 @@ function Video() {
   return (
     <>
       <section className="bg-image background-img intro-video">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-6">
-              <div className="video-promo-content mt-3 text-center">
-                <a
-                  href="https://player.vimeo.com/video/299432146"
-                  className="popup-youtube video-play-icon d-inline-block"
-                >
-                  <span className="ti-control-play"></span>{' '}
-                </a>
-                <h5 className="mt-4 text-white">Confira nosso vídeo</h5>
-              </div>
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="video-promo-content mt-3 text-center">
+              <a
+                href="https://player.vimeo.com/video/299432146"
+                className="popup-youtube video-play-icon d-inline-block"
+              >
+                <span className="ti-control-play"></span>{' '}
+              </a>
+              <h5 className="mt-4 text-white intro-video-text">Confira nosso vídeo</h5>
             </div>
           </div>
         </div>
